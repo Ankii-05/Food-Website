275389d30dda4b4dd39e9c86ca0688a5e7ccc3be
Ankii-05 <ankitkushwah6195@gmail.com>
2025-05-16 15:55:01 +0530

    Project restart

--- food recipe/src/App.jsx
+++ food recipe/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+import './App.scss'
+// react router-dom
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from 'react-router-dom'
+
+import {Home, MealDetails, Error, Category} from './pages/index'
+import Header from "./components/Header/Header"
+import Sidebar from './components/Sidebar/Sidebar';
 
 const App = () => {
   return (
-    <div>
+    <BrowserRouter>
+    <Header/>
+    <Sidebar/>
+    <Routes>
+      <Route path='/' element = {<Home/>} />
+      <Route path='/meal/:id' element = {<MealDetails/>} />
+      <Route path='/meal/category/:name' element = {<Category/>} />
+      <Route path='/meal/category/:name' element = {<Category/>} />
+      <Route path='*' element = {<Error/>} />
       
-    </div>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
